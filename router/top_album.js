50aae772d1c9f00d4f23e93502e766056163fd8a
Misxiaoxiao <1102820661@qq.com>
2019-02-22 10:58:48 +0800

    2.22 添加新碟上架接口

---
+++ router/top_album.js
@@ -0,0 +1,22 @@
+// 新碟上架
+module.exports = (req, res, createWebAPIRequest, request) => {
+  const cookie = req.get("Cookie") ? req.get("Cookie") : "";
+  const data = {
+    offset: req.query.offset || 0,
+    total: true,
+    limit: req.query.limit || 50,
+    area: req.query.type,
+    csrf_token: ""
+  };
+  createWebAPIRequest(
+    "music.163.com",
+    "/weapi/album/new",
+    "POST",
+    data,
+    cookie,
+    music_req => {
+      res.send(music_req);
+    },
+    err => res.status(502).send("fetch error")
+  );
+};
